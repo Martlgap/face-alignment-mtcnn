c61d247f7bfe8068b88ef22594403ba550dee758
Martin Knoche <Martin.Knoche@tum.de>
2021-06-17 20:48:12 +0200

    ready to release v0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    version="0.1",
+    version="0.2",
     name="face-alignment-mtcnn",
     packages=find_packages(),
     url="https://github.com/Martlgap/face-alignment",
